d8a15f3d114c9367156f4f2360d9552b5d314e31
Cody <161258462+cwitzer@users.noreply.github.com>
2025-03-01 19:50:44 -0500

    Create my_code.py

---
+++ .github/workflows/my_code.py
@@ -0,0 +1,24 @@
+
+
+list_1 = [0,1,2,3,4,5,6,7,8,9,10]
+
+
+
+
+
+a = int(input("Input.1: "))
+b = int(input("Input.2: "))
+
+
+
+
+
+
+def operations():
+    math = print("\nMath:",a + b)
+    subtraction = print("Subtraction:",a - b)
+    multiplication = print("Multiplication:",a * b)
+    division = print("Division:",a / b)
+    
+operations()
+
